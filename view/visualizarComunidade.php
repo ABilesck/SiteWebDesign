6c548743432fab5519a8590d5e4c0e8a3f891ead
alancrz <65604582+alancrz@users.noreply.github.com>
2020-10-24 23:24:36 -0300

    Atualizado para AC2

--- view/visualizarComunidade.php
+++ view/visualizarComunidade.php
@@ -25,70 +25,123 @@ $url = 'http://localhost/sitewebdesign/view/visualizarPerfil.php?id=';
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-BR" class="htm">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Comunidades</title>
+    <link rel="stylesheet" href="./assets/css/styleVisualizarComunidade.css">
+    <link rel="icon" type="image/gif" href="./assets/img/favicon.png">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
-<body>
 
-    <h1>Comunidade</h1>
-    <br/>
-    <label>Nome: </label><?php echo $dados['nome'] ?>
-    <br/>
-    <label>Tema: </label><?php echo $dados['tema'] ?>
-    <br/>
-    <label>Descrição: </label><?php echo $dados['descricao'] ?>
+<body class="bd">
+    <header class="formComunidade">
+        <div class="card">
+            <div class="card-top">
+                <div class="texto">
+                    <img class="imgLogin" src="./assets/img/CodificandoLogo.png" alt="Logo">
+                    <h2 class="titulo">Codificando</h2>
+                </div>
+                <div class="botoes">
+                    <a href="login.html">
+                        <input class="botao" type="button" value="Fazer Login"></input>
+                    </a>
+                </div>
+            </div>
+        </div>
 
-    <h2>Membros da comunidade</h2>
-    <table>
-        <tr>
-            <th>Nome de usuario</th> <th>Bio</th>
-        </tr>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <img src="./assets/img/favicon.png" width="30" height="30" class="d-inline-block align-top pad" alt="Logo" loading="lazy">
+            <a class="navbar-brand" href="./index.html">Codificando</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="./visualizarPerfil.php">Perfis <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="./visualizarComunidade.php">Comunidades</a>
+                    <a class="nav-link" href="#">Sobre</a>
+                </div>
+            </div>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Digite a sua pesquisa" aria-label="Search">
+                <button class="bottao my-2 my-sm-0" type="submit">Procurar</button>
+            </form>
+        </nav>
+    </header>
 
-        <?php 
 
-        $membros = $perfisDaComunidade->LerPorComunidade();
-        
-        foreach ($membros as $m) { ?>
-            
-            <tr>
-                <td>  <?php echo $m['usuario'] ?>  </td>
-                <td>  <?php echo $m['bio'] ?>  </td>
-            </tr>
+    <section class="escopo">
+        <article class="card">
 
-        <?php } ?>
-    </table>
+        <div class="comunidadeLista">
+            <ul class="list-group lista">
+                <li class="list-group-item active listaLi">Comunidade</li>
+                <li style="margin-top: 0px;" class="list-group-item listaLi">Nome</li> <?php echo $dados['nome'] ?>
+                <li class="list-group-item listaLi">Assunto</li> <?php echo $dados['tema'] ?>
+                <li class="list-group-item listaLi">Descrição</li> <?php echo $dados['descricao'] ?>
+            </ul>
+        </div>
 
-    <p>
+        <div class="comunidadeLista">
+            <ul class="list-group lista">
+                <li class="list-group-item active listaLi">Membros da comunidade</li>
+                <li style="margin-top: 0px;" class="list-group-item listaLi">Nome de usuario</li> 
+                <?php
+                $membros = $perfisDaComunidade->LerPorComunidade();
+                foreach ($membros as $m) { ?>
+                         <?php echo $m['usuario'] ?> 
+                <?php } ?>
 
-    <fieldset>
-        <legend>Novo post</legend>
-        <form action="../DAO/post/novoPost.php" method="POST">
-            <input type="text" name="texto" id="texto"/> <input type="submit" value="Postar"/>
-            <input type="hidden" id="autor" name="autor" value="<?php echo $_SESSION['Id_Perfil'] ?>">
-            <input type="hidden" id="comunidade" name="comunidade" value="<?php echo $id_com ?>">
-        </form>
-    </fieldset>
+                <li class="list-group-item listaLi">Biografia</li>
+                <?php
+                $membros = $perfisDaComunidade->LerPorComunidade();
+                foreach ($membros as $m) { ?>
+                         <?php echo $m['bio'] ?> 
+                <?php } ?> 
+            </ul>
+        </div>
+            <p>
 
-    <h2>Todos os posts</h2>
+                <fieldset>
+                    <legend>Novo post</legend>
+                    <form action="../DAO/post/novoPost.php" method="POST">
+                        <input type="text" name="texto" id="texto" /> <input type="submit" value="Postar" />
+                        <input type="hidden" id="autor" name="autor" value="<?php echo $_SESSION['Id_Perfil'] ?>">
+                        <input type="hidden" id="comunidade" name="comunidade" value="<?php echo $id_com ?>">
+                    </form>
+                </fieldset>
 
-    <?php
-    
-            $TodosOsPosts = $posts->PostsDaComunidade();
-            if(count($TodosOsPosts) != 0)
-            foreach ($TodosOsPosts as $p) { ?>
-                
-                <article>
-                    <h3><a href="<?php echo $url . $p['idAutor'] ?>"> <?php echo $p['autor'] ?> </a></h3>
-                    <br/>
-                    <?php echo $p['texto'] ?>
-                    <br/>
-                </article>
-                <br/>
+                <h2>Todos os posts</h2>
 
-           <?php } ?>
-    
+                <?php
+
+                $TodosOsPosts = $posts->PostsDaComunidade();
+                if (count($TodosOsPosts) != 0)
+                    foreach ($TodosOsPosts as $p) { ?>
+
+                    <article>
+                        <h3><a href="<?php echo $url . $p['idAutor'] ?>"> <?php echo $p['autor'] ?> </a></h3>
+                        <br />
+                        <?php echo $p['texto'] ?>
+                        <br />
+                    </article>
+                    <br />
+
+                <?php } ?>
+        </article>
+    </section>
+
+    <footer class="ftr">
+        <p>This site is an academic project</p>
+        <p>Copyright © 2020 - All rights reserved - Codificando</p>
+    </footer>
+
+
 </body>
+
 </html>
